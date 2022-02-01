f51b3cd08f9f97b6125b71b4e7c40c41a1846d7c
shadowk1337 <r_fyodorov2001@mail.ru>
2022-02-01 10:18:44 +0300

    Minor fix

--- src/testnetworkmanager.cpp
+++ src/testnetworkmanager.cpp
@@ -4,19 +4,19 @@
 #include <QNetworkCookieJar>
 
 TestNetworkManager::TestNetworkManager(QObject *parent)
-    : QObject(parent),
-      _cookiesHandler(new CookiesHandler),
+    : QObject(parent), _cookiesHandler(new CookiesHandler),
       _curlManager(new QtCurlManager) {}
 
 TestNetworkManager::~TestNetworkManager() {}
 
-TestNetwork::RequestResult TestNetworkManager::send(
-    const TestNetwork::RequestPackage &package) {
+TestNetwork::RequestResult
+TestNetworkManager::send(const TestNetwork::RequestPackage &package) {
   _curlManager->setPackage(package);
-  _curlManager->sendRequest();
-  if (true) {
+  if (_curlManager->sendRequest()) {
+    qDebug() << "SUCCESS";
     return TestNetwork::SUCCESS;
   } else {
+    qDebug() << "FAILURE";
     return TestNetwork::FAILURE;
   }
 }
@@ -25,29 +25,34 @@ QtCurlManager::QtCurlManager(QObject *parent) : QObject(parent) {
   _curl.setTextCodec("Windows-1251");
 }
 
+TestNetwork::RequestResult QtCurlManager::sendRequest() {
+  return sendPostRequest() && sendGetRequest() ? TestNetwork::SUCCESS
+                                               : TestNetwork::FAILURE;
+}
+
 TestNetwork::RequestResult QtCurlManager::sendPostRequest() {
   QtCUrl::Options options;
-//  options[CURLOPT_URL] = _package.url;
-//  options[CURLOPT_POST] = true;
-//  QUrlQuery urlQuery;
-//  urlQuery.addQueryItem("username", _package.login);
-//  urlQuery.addQueryItem("password", _package.password);
+  options[CURLOPT_URL] = _package.url;
+  options[CURLOPT_POST] = true;
+  QUrlQuery urlQuery;
+  urlQuery.addQueryItem("username", _package.login);
+  urlQuery.addQueryItem("password", _package.password);
 //  options[CURLOPT_POSTFIELDS] = QUrl(urlQuery.toString());
-//  options[CURLOPT_SSL_VERIFYHOST] = false;
-//  options[CURLOPT_SSL_VERIFYPEER] = false;
-//  options[CURLOPT_COOKIEJAR] = QDir::currentPath() + "/cookie.txt";
-//  options[CURLOPT_USERAGENT] = _userAgent;
-//  options[CURLOPT_REFERER] = "";  // TODO
-//  options[CURLOPT_FOLLOWLOCATION] = false;
+  options[CURLOPT_POSTFIELDS] = "https://account.mail.ru";
+  options[CURLOPT_SSL_VERIFYHOST] = false;
+  options[CURLOPT_SSL_VERIFYPEER] = false;
+  options[CURLOPT_COOKIEJAR] = QDir::currentPath() + "/cookie.txt";
+  options[CURLOPT_USERAGENT] = _userAgent;
+  options[CURLOPT_REFERER] = "https://account.mail.ru"; // TODO
+  options[CURLOPT_FOLLOWLOCATION] = false;
 
   QString result = _curl.exec(options);
   if (_curl.lastError().isOk()) {
     qDebug() << result;
     return TestNetwork::SUCCESS;
   } else {
-    qDebug() << QString("ERROR: %1\nBUFFER: %2")
-                    .arg(_curl.lastError().text())
-                    .arg(_curl.errorBuffer());
+    qDebug() << QString("ERROR");
+    qDebug() << QString("BUFFER: %1").arg(_curl.errorBuffer());
     return TestNetwork::FAILURE;
   }
 }
